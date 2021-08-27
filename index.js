8e947e2d292241cd271c524b12cfe3b19c0dbd9f
Noah van der Aa <ndvdaa@gmail.com>
2021-08-27 10:58:37 +0200

    Remove double semicolon

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { Client, Collection } = require('discord.js');
 const fs = require('fs');
 
-const config = require('./config.json');;
+const config = require('./config.json');
 
 const client = new Client({
 	intents: [
